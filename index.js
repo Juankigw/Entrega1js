6ca0cede26abbc9df209ec497770cca116926398
Juan Carlos Silva <juanki_gw@yahoo.com.ar>
2022-10-05 02:31:01 -0300

    algunos detalles

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ let pizza1= new pizza(1, "Muzzarela", ["salsa", "muzzarela", "aceitunas"], 550);
 let pizza2= new pizza(2, "Napolitana", ["salsa", "muzzarela", "aceitunas", "tomate"], 700);
 let pizza3= new pizza(3, "Calabresa", ["salsa", "muzzarela", "aceitunas", "salame"], 750);
 let pizza4= new pizza(4, "Roquefort", ["salsa", "muzzarela", "aceitunas", "roquefort"], 800);
-let pizza5= new pizza(5, "Fugazeta", ["salsa", "muzzarela", "aceitunas", "cebolla"], 500);
+let pizza5= new pizza(5, "Fugazeta", ["salsa", "muzzarela", "aceitunas", "cebolla"], 700);
 let pizza6= new pizza(6, "Especial", ["salsa", "muzzarela", "aceitunas", "jamon", "morron"], 750);
 
 const arrayPizzas = [];
@@ -25,11 +25,10 @@ for (let index = 0; index < arrayPizzas.length; index++) {
 console.log("");
 for (let index = 0; index < arrayPizzas.length; index++) {
     if (arrayPizzas[index].precio<600) {
-        console.log("esta pizza vale menos de $600");
-        console.log(arrayPizzas[index].nombre);
+        console.log("La pizza ", arrayPizzas[index].nombre, " vale menos de $600");
+        console.log("");
     }
 }
-console.log("");
 console.log("A continuacion se muestra una lista de nuestras pizzas con sus respectivos precios");
 for (let index = 0; index < arrayPizzas.length; index++) {
     console.log(arrayPizzas[index].nombre,arrayPizzas[index].precio );
